959946d7110a8cd716aba25bbe6196f3034a495b
Aidan <astaff21@charlotte.edu>
2025-04-30 23:23:00 -0400

    jobs tab and jobs postings

--- base/views.py
+++ base/views.py
@@ -10,7 +10,24 @@ from .forms import RoomForm, UserForm, MyUserCreationForm
 # Jobs Page View
 def jobsPage(request):
     # Add your logic here (e.g., querying jobs from a database)
-    return render(request, 'base/jobs.html')
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+
+    topics = Topic.objects.all()[0:5]
+    room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+
+    context = {
+        'rooms': rooms, 
+        'topics': topics, 
+        'room_count': room_count,
+        'room_messages': room_messages
+    }
+    return render(request, 'base/jobs.html', context)
 
 # My Jobs and Reviews Page View
 def myJobsReviewsPage(request):
