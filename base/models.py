a6adea3fa5c194f2edec1834b14b2f64e3174b47
NathanStinnett9 <nstinnet@charlotte.edu>
2025-04-30 23:08:16 -0400

    JobImage

--- base/models.py
+++ base/models.py
@@ -32,6 +32,7 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='room_images/', null=True, blank=True)  # Add image field
     participants = models.ManyToManyField(User, related_name='participants', blank=True)
     update = models.DateTimeField(auto_now=True)
     create = models.DateTimeField(auto_now_add=True)
@@ -41,7 +42,6 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
-    
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
